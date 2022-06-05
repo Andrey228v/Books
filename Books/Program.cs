400d51df9162764a03a2fc3b821d987292843c81
gysev003 gysev003 <gysev003@mail.ru>
2022-06-05 12:00:49 +0300

    Контейнер сервисов

---
+++ Books/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Books
+{
+    class Program
+    {
+
+        // Создали вторую точку в приложении. Компелятор будет ругаться на это.
+        // Поэтому в свойствах проекта необходимо указать какая точка будет основной
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) => Host
+            .CreateDefaultBuilder(args)
+            .ConfigureServices(App.ConfigureServices);
+    }
+}
